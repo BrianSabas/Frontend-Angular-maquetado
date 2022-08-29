51df73eb668ad73a1f94d87943973dcbbacc2137
BrianSabas <braianns@hotmail.com>
2022-08-29 20:10:29 -0300

    Se agregó un progress-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
